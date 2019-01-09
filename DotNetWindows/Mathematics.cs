8bd4a3d8d6408c048a0bccdd8dfd273778a1020b
Paulo Masaru Meneghel Jitsukawa <pjitsukawa@dataweb.com.br>
2019-01-09 17:01:38 -0200

    Adição de métodos de arredondamento.

---
+++ DotNetWindows/Mathematics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DotNetWindows
+{
+    public static class Mathematics
+    {
+        /// <summary>
+        /// Arredonda um número por truncamento.
+        /// </summary>
+        /// <param name="mantissa">Quantidade de casas decimais.</param>
+        public static decimal Truncate(this decimal number, uint mantissa)
+        {
+            var p = (decimal)Math.Pow(10, mantissa);
+            return Math.Truncate(number * p) / p;
+        }
+
+        /// <summary>
+        /// Arredonda um número por truncamento.
+        /// </summary>
+        /// <param name="mantissa">Quantidade de casas decimais.</param>
+        public static double Truncate(this double number, uint mantissa)
+        {
+            var p = Math.Pow(10, mantissa);
+            return Math.Truncate(number * p) / p;
+        }
+
+        /// <summary>
+        /// Arredonda um número por arredondamento científico.
+        /// </summary>
+        /// <param name="mantissa">Quantidade de casas decimais.</param>
+        public static decimal Round(this decimal number, uint mantissa)
+        {
+            var p = (decimal)Math.Pow(10, mantissa);
+            return Math.Round(number * p) / p;
+        }
+
+        /// <summary>
+        /// Arredonda um número por arredondamento científico.
+        /// </summary>
+        /// <param name="mantissa">Quantidade de casas decimais.</param>
+        public static double Round(this double number, uint mantissa)
+        {
+            var p = Math.Pow(10, mantissa);
+            return Math.Round(number * p) / p;
+        }
+    }
+}
